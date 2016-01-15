ce8e02cb1c37e0277df18f8182141316f350d482
berashevich <shepard8152@gmail.com>
2016-01-15 13:06:42 +0300

    connecting DB to RssBusiness

--- app/src/main/java/hackspace/testtask/com/testtask/rss/RssBusiness.java
+++ app/src/main/java/hackspace/testtask/com/testtask/rss/RssBusiness.java
@@ -17,11 +17,66 @@ public class RssBusiness {
     public static List<RssItem> getRssItems(Context context) {
         rssItems = new ArrayList<>();
 
+        RssDbHelper mDbHelper = new RssDbHelper(context);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        Cursor c = db.query(RssEntry.TABLE_NAME, null, null, null, null, null, null);
+
+        if (c.moveToFirst()) {
+            int imageColIndex = c.getColumnIndex(RssEntry.COLUMN_NAME_IMAGE);
+            int titleColIndex = c.getColumnIndex(RssEntry.COLUMN_NAME_TITLE);
+            int descriptionColIndex = c.getColumnIndex(RssEntry.COLUMN_NAME_DESCRIPTION);
+
+            do {
+                rssItems.add(
+                        new RssItem(
+                                c.getString(titleColIndex),
+                                c.getString(descriptionColIndex),
+                                c.getString(imageColIndex)
+                        ));
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        mDbHelper.close();
 
         return rssItems;
     }
 
+    public static boolean setRssItems(List<RssItem> rssItems, Context context) {
+        boolean isBdUpdated = true;
+        RssDbHelper mDbHelper = new RssDbHelper(context);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        db.delete(RssEntry.TABLE_NAME, null, null);
+
+        ContentValues values = new ContentValues();
+        for (int i = 0; i < rssItems.size(); i++) {
+            values.put(RssEntry.COLUMN_NAME_IMAGE, rssItems.get(i).getImage());
+            values.put(RssEntry.COLUMN_NAME_TITLE, rssItems.get(i).getTitle());
+            values.put(RssEntry.COLUMN_NAME_DESCRIPTION, rssItems.get(i).getDescription());
+
+            long newRowId = db.insert(
+                    RssEntry.TABLE_NAME,
+                    null,
+                    values);
+            if (newRowId == -1) {
+                isBdUpdated = false;
+            }
+        }
+
+        mDbHelper.close();
+        return isBdUpdated;
+    }
+
     public static List<RssItem> delete(RssItem rssItem, Context context) {
+        RssDbHelper mDbHelper = new RssDbHelper(context);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+
+        String selection = RssEntry.COLUMN_NAME_TITLE + " LIKE ?";
+        String[] selectionArgs = { rssItem.getTitle() };
+        int delCount = db.delete(RssEntry.TABLE_NAME, selection, selectionArgs);
 
         rssItems.remove(rssItem);
         return rssItems;
